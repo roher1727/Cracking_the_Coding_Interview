5ff042a91f38b7c89a84fc8e3a8153c73cf365cc
Pato <30847666+patoba@users.noreply.github.com>
2020-07-11 14:43:48 -0500

    Add files via upload

---
+++ sesion1/sesion1.h
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 1.x
+
+bool is_unique(string s);
+bool check_permutation(string s1, string s2);
+string URLify(string s);
+bool palindrome_permutation(string s);
+bool one_away(string s1, string s2);
+string string_compression(string s);
+int** rotate_matrix(int r, int c, int **m); 
+int** zero_matrix(int r, int c, int **m); 
+bool string_rotation(string s1, string s2);
+
+// 2.x
+
+list<int> remove_dups(list<int> lista);
+int return_Kth_to_last(int k, list<int> lista);
+list<int> delete_middle_node(int indice, list<int> lista);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//subir el sabado soluciones a omegaup
+//domingo soluciones a git
+//
