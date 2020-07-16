4eb11e4ce04b6e1300bd7173da609b17c7942dfc
Pato <30847666+patoba@users.noreply.github.com>
2020-07-16 14:11:05 -0500

    Update escribir.cpp

--- Generador_Soluciones/escribir.cpp
+++ Generador_Soluciones/escribir.cpp
@@ -8,6 +8,6 @@ using namespace std;
 void escribir_archivo(string contenido, string nombre_archivo){
     ofstream archivo;
     archivo.open (nombre_archivo);
-    archivo << contenido;
+    archivo << contenido << endl;
     archivo.close();
 }
