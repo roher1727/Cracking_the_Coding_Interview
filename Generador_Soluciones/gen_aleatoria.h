19912ef1fd0737cd3953eb09e011d8aafc3db77d
Pato <30847666+patoba@users.noreply.github.com>
2020-07-17 19:09:25 -0500

    Mas funciones generadoras aleatorias

--- Generador_Soluciones/gen_aleatoria.h
+++ Generador_Soluciones/gen_aleatoria.h
@@ -10,3 +10,19 @@ char char_aleatorio();
 
 //cadena aleatoria
 string cadena_aleatoria(int tamano);
+
+int numero_aleatorio_alrededor_de(double media, double desv_estandar);
+char char_aleatorio_alrededor_de(char c, double desv = 4);
+string string_alrededor_de(int n, char c, double desv = 4);
+
+int* arreglo_uni_aleatorio(int n, int a, int b);
+int** arreglo_bi_aleatorio(int ren, int col, int a, int b);
+int ***arreglo_tri_aleatorio(int n, int ren, int col, int a, int b);
+
+void liberar_arreglo_uni(int *arr);
+void liberar_arreglo_bi(int ren, int **arr);
+void liberar_arreglo_tri(int n, int ren, int ***arr);
+
+string cadena_arreglo_uni(int n, int* arr);
+string cadena_arreglo_bi(int r, int c, int **arr);
+string cadena_arreglo_tri(int n, int r, int c, int ***arr);
